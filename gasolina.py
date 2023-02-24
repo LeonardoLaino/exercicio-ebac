cdd8795d457e0dad014a5f18b6fc475410643937
${GITHUB_USER} <${GITHUB_MAIL}>
2023-02-23 21:13:05 -0300

    arquivo gasolina.py alterado: adicionadas novas funcionalidades ao gráfico gerado. gasolina.png: resultado das alterações no arquivo gasolina.py.

--- gasolina.py
+++ gasolina.py
@@ -1,29 +1,47 @@
-# importando as bibliotecas necessárias
 import pandas as pd
-import seaborn as sns
-from matplotlib.pyplot import savefig
+from matplotlib.pyplot import savefig,figure,subplots,show
 
 # Lendo os dados
 df = pd.read_csv(filepath_or_buffer= 'gasolina.csv', sep = ',', header= 0)
 
-# Criando o gráfico
-obj = sns.lineplot(
-    data = df,
-    x = 'dia',
-    y = 'venda',
-    color = 'magenta'
-)
+fig,ax = subplots(figsize= (12,8))
 
-# Editando título do gráfico e dos eixos
-obj.set_title("Valor de Venda da Gasolina x Dias")
-obj.set_xlabel('Dias')
-obj.set_ylabel('Valor da Gasolina')
+ax.plot('dia', 'venda', data= df)
+
+ax.set_title("Valor de Venda da Gasolina x Dias")
+ax.set_xlabel('Dias')
+ax.set_ylabel('Valor da Gasolina')
 
 # Editando os rótulos dos eixos
-obj.set_xticks(df['dia'])
+ax.set_xticks(df['dia'])
 
 # Colocando linhas de grade
-obj.grid()
+ax.grid()
+
+# Colocando o valor de cada ponto
+for x in zip(df['dia'], df['venda']):
+    ax.annotate(
+        'R$'+str(x[1]),
+        xy= x,
+        horizontalalignment = 'left',
+        fontsize = 12,
+        color = 'darkgreen',
+        style = 'oblique'
+        )
+
+# Pegando o Menor valor de venda registrado no período
+menor = df['venda'].min()
+
+# Pegando o índice
+min_idx = df.query('venda == @menor').index.to_list()
+
+# Criando uma seta para apontar no gráfico o menor valor registrado
+ax.annotate(
+    text='Menor Valor no Período', 
+    xy= (int(df['dia'].iloc[min_idx]),float(df['venda'].iloc[min_idx])), 
+    xytext=(5.5,5), 
+    arrowprops= dict(facecolor = 'red', arrowstyle= '->')
+    )
 
 # Salvando o gráfico gerado
 savefig('gasolina.png')
